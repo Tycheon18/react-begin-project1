5f046508e8cb112a32cccf3e29069b381d3a9093
Tycheon18 <joong0818@gmail.com>
2025-06-25 03:01:36 +0900

    add components to understand component flow

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
 import './App.css';
-import {useState} from "react";
+import { useRef, useEffect, useState } from "react";
 import Viewer from "./component/Viewer";
 import Controller from "./component/Controller"
+import Even from "./component/Even"
 
 function App() {
 
   const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const didMountRef = useRef(false);
 
   const handleSetCount = (value) => {
     setCount(count + value);
   }
+  const handleChangeText = (e) => {
+    setText(e.target.value);
+  }
 
+  useEffect(() => {
+    if(!didMountRef.current)
+    {
+      didMountRef.current = true;
+      return;
+    } else {
+    console.log("컴포넌트 업데이트!");
+    }
+  }); 
+
+  useEffect(() => {
+    console.log("컴포넌트 마운트");
+  } , []);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      console.log("깜빡");
+    } , 1000);
+
+    return () => {
+      console.log("클린업");
+      clearInterval(intervalId);
+    }
+  });
+  
   return (
     <div className="App">
       <h1>Simple Counter</h1>
       <section>
+        <input value={text} onChange={handleChangeText} />
+      </section>
+      <section>
         <Viewer count={count}/>
+        {count % 2 === 0 && <Even />}
       </section>
       <section>
         <Controller handleSetCount = {handleSetCount}/>
